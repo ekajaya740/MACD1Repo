36894354ce305d5547ea082e2059408fc54f5b34
ekajaya740 <34807386+ekajaya740@users.noreply.github.com>
2019-04-11 21:38:15 +0800

    Update01

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
  <head>
  <Title>Registration Form</Title>
  <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+  @import url('https://fonts.googleapis.com/css?family=Mukta');
+ 	body { background-color: #000; border-top: solid 10px #000;
+ 	    color: #fff; font-size: .85em; margin: 20; padding: 20;
+ 	    font-family: 'Mukta', sans-serif;
  	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+ 	h1, h2, h3,{ color: #fff; margin-bottom: 0; padding-bottom: 0; }
  	h1 { font-size: 2em; }
  	h2 { font-size: 1.75em; }
  	h3 { font-size: 1.2em; }
